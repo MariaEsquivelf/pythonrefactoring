15b466d167725a8d61973c5ade3e1e47393935b4
MariaEsquivelf <131416497+MariaEsquivelf@users.noreply.github.com>
2023-08-30 23:48:30 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,34 +1,38 @@
 def introducir_puntos_comentarios():
     while True:
-        print('Por favor, introduzca una puntuación en una escala de 1 a 5')
-        point = input()
+        point = input('Por favor, introduzca una puntuación en una escala de 1 a 5: ')
 
         if point.isdecimal():
             point = int(point)
             if 1 <= point <= 5:
-                print('Introduzca sus comentarios.')
-                comment = input()
+                comment = input('Introduzca sus comentarios: ')
                 post = f'Puntos: {point} Comentarios: {comment}'
-                with open("data.txt", 'a') as file_pc:
-                    file_pc.write(f'{post}\n')
+                guardar_post(post)
                 break
             else:
                 print('Indíquelo en una escala de 1 a 5')
         else:
             print('Introduzca los puntos de valoración como números')
 
+def guardar_post(post):
+    with open("data.txt", 'a') as file_pc:
+        file_pc.write(f'{post}\n')
+
 def mostrar_resultados():
     print('Resultados hasta la fecha.')
     with open("data.txt", "r") as read_file:
         print(read_file.read())
 
+def seleccionar_proceso():
+    print('Seleccione el proceso que desea aplicar')
+    print('1: Introducir puntos de evaluación y comentarios')
+    print('2: Comprobar los resultados hasta ahora')
+    print('3: Terminar')
+    return input()
+
 def main():
     while True:
-        print('Seleccione el proceso que desea aplicar')
-        print('1:Introducir puntos de evaluación y comentarios')
-        print('2:Comprobar los resultados hasta ahora.')
-        print('3:Terminar.')
-        num = input()
+        num = seleccionar_proceso()
 
         if num.isdecimal():
             num = int(num)
